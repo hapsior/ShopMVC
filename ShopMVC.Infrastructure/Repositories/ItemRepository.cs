ee7f193346825d8ffd0bdea4b0e75079bfb4010f
hapsior <hapsiorek@gmail.com>
2022-10-24 22:22:34 +0200

    Added ItemRepository

---
+++ ShopMVC.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,52 @@
+﻿using ShopMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Infrastructure.Repositories
+{
+    public class ItemRepository
+    {
+        private readonly Context _context;
+
+        public ItemRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void DeleteItem(int itemId)
+        {
+            var item = _context.Items.Find(itemId);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                _context.SaveChanges();
+            }
+        }
+
+        public int AddItem(Item item)
+        {
+            _context.Add(item);
+            // return _context.SaveChanges(); //To check how works
+            _context.SaveChanges();
+            return item.Id;
+        }
+
+        public IQueryable<Item> GetItemsByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return _context.Items;
+            }
+
+            return _context.Items.Where(x => x.Name.Contains(name));
+        }
+
+        public IQueryable<Item> GetItemById(int itemId)
+        {
+            return _context.Items.Where(x => x.Id == itemId);
+        }
+    }
+}

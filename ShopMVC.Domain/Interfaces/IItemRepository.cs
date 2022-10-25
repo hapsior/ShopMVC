dc73fdf6c2567449df7ad4c23e11aff8fcf78c9f
hapsior <hapsiorek@gmail.com>
2022-10-25 22:54:09 +0200

    Created IItemRepository

---
+++ ShopMVC.Domain/Interfaces/IItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using ShopMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        public void DeleteItem(int itemId);
+        public int AddItem(Item item);
+        public IQueryable<Item> GetItemsByName(string name);
+        public IQueryable<Item> GetItemById(int itemId);
+    }
+}
